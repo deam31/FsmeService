b32f512a795ec3b222928d92ead1695a89984023
extadoaa <extadoaa@SB0048613.addm.ads.brm.pri>
2016-12-28 12:36:10 +0000

    First commit. Project basics.

---
+++ src/main/java/deam/inc/fsme/App.java
@@ -0,0 +1,13 @@
+package deam.inc.fsme;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
